f7fd85218dc9b0f1e5a6ee915368f7f6bf0c9893
jeevanjacob55 <jeevanjacob55@gmail.com>
2025-06-30 15:36:42 +0530

    added environment variables

--- main.py
+++ main.py
@@ -12,11 +12,12 @@ from werkzeug.security import generate_password_hash, check_password_hash
 # Import your forms from the forms.py
 from forms import CreatePostForm,RegisterForm,LoginForm,CommentForm
 from typing import List
-
-
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 
@@ -58,8 +59,7 @@ def admin_only(f):
 # CREATE DATABASE
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///posts.db'
-app.secret_key = "it_is_my_secret_key"
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('SQLALCHEMY_URI')
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
